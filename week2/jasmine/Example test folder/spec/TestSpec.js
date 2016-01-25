04ec611e16392d280e4a231553856fe699b49a14
Owen TM <oturnermajor@gmail.com>
2016-01-25 12:53:52 +0000

    example tests

---
+++ week2/jasmine/Example test folder/spec/TestSpec.js
@@ -0,0 +1,30 @@
+describe("The example function should", function() {
+  it("return the string 'example'", function() {
+    var actual = example();
+    expect(actual).toBe("example");
+    expect(actual).not.toBe("something else");
+  });
+});
+
+describe("The double function", function() {
+    var arr = [1, 2, 4];
+    var actual = double(arr);
+    it("should return a simple array, doubled", function (){
+        expect(actual).toEqual([2, 4, 8]);
+        expect(actual).not.toEqual("a string");
+    });
+
+    var arr2 = [1.1, 2.2, 3.3];
+    var actual2 = double(arr);
+    it("should work for non-integers", function() {
+        expect(actual2).toEqual([2.2, 4.4, 6.6]);
+        expect(actual2).not.toEqual("a string");
+    });
+
+    var arr3 = [-1, -2, -3];
+    var actual3 = double(arr3);
+    it("work for negative numbers", function(){
+        expect(actual3).toEqual([-2, -4, -6]);
+        expect(actual3).not.toEqual("");
+    });
+});
