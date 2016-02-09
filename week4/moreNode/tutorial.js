0ee19157431a591622fc33aec9b91101ec87afec
Owen TM <oturnermajor@users.noreply.github.com>
2016-02-09 16:01:39 +0000

    Update tutorial.js

--- week4/moreNode/tutorial.js
+++ week4/moreNode/tutorial.js
@@ -2,22 +2,23 @@
 
 
 
-// Set up a fs.readFile function to read the people.json file. Convert the 
-// data from the file into a javascript object using JSON.parse, and 
-// assign this object to the variable 'result'.
+// 1. Use the fs.readFile function below to read the people.json file 
+// 2. Convert the data from the file into a javascript object using  
+// JSON.parse and assign this object to the variable 'result'.
 
-fs.readFile('people.json', 'utf8', function(err, data){
+fs.readFile(filePathParameter, optionsOrEncodingParameter, function(err, data){
 
 	
 
-// Make an array of the lastnames of the people in your object, 
+// 3. Make an array of the lastnames of the people in your object, 
 // ordered alphabetically.
 
 
 
-// Make an object called nameObject with a the property lastName, which should
-// be the array you have made. Make a variable called returnObject, which stringifys nameObject.
-// Finally, use fs.writeFile to write return object to a new file called nameFile.json.
+// 4. Make an object called nameObject with a the property lastName, which should
+// be the array you have made. 
+// 5. Make a variable called returnObject, which stringifys nameObject.
+// 6. Finally, use fs.writeFile to write return object to a new file called nameFile.json.
 
 
 
