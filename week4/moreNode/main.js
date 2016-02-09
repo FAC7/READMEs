8600077707bd8682b5c27c7b0f2e5620f4256033
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-02-09 14:58:54 +0000

    commiting the second draft of the tutorial on node file systems.

--- week4/moreNode/main.js
+++ week4/moreNode/main.js
@@ -1,16 +1,28 @@
 var fs = require("fs");
-
-console.log("Going to write into existing file");
-fs.writeFile('input.txt', 'changestofile!',  function(err) {
-   if (err) {
-       return console.error(err);
-   }
-   console.log("Data written successfully!");
-   // console.log("Let's read newly written data");
-   // fs.readFile('input.txt', function (err, data) {
-   //    if (err) {
-   //       return console.error(err);
-   //    }
-   //    console.log("Asynchronous read: " + data.toString());
-   // });
+// read file and store data as variable
+var fileData;
+fs.readFile('people.json', "utf8", function(err, data) {
+  if(err){
+    return console.error(err);
+  }
+  else {
+    fileData = JSON.parse(data);
+  }
 });
+
+// take data and store it in another file as a Json object
+
+// console.log("Going to write into existing file");
+// fs.writeFile('input.txt', 'changestofile!',  function(err) {
+//    if (err) {
+//        return console.error(err);
+//    }
+//    console.log("Data written successfully!");
+//    // console.log("Let's read newly written data");
+//    // fs.readFile('input.txt', function (err, data) {
+//    //    if (err) {
+//    //       return console.error(err);
+//    //    }
+//    //    console.log("Asynchronous read: " + data.toString());
+//    // });
+// });
