ebee2482e83efe700dc4172768edca746669e5f4
RobStallion <Robertefrancis18@gmail.com>
2016-03-15 18:37:08 +0000

    add tutorial file with react files

---
+++ week9/PropsAndState/tutorial/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: {
+    javascript: './app.js',
+    html: './index.html'
+  },
+  output: {
+    filename: 'app.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
+};
